0a083579c5d015c17d6c4407497d2c18aa111d74
Constanza Mardones <coty.tiff@gmail.com>
2021-01-18 11:10:53 -0300

    agregando request

--- web.page.rb
+++ web.page.rb
@@ -3,4 +3,4 @@ require_relative 'components'
 
 nasa_api = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=BCvNKRQ0riiQJzWZZyitmuw0fnbbwuBiYzeodJwQ')
 
-File.write('./index.html', template(nasa_api))
+File.write('./index.html', build_web_page(nasa_api))
